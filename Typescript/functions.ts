80327f3a49ca36159e9b33b366b98afccc9921cc
Praveen Roy <pkroy0505@gmail.com>
2026-02-12 22:57:57 +0530

    added function

--- Typescript/functions.ts
+++ Typescript/functions.ts
@@ -93,3 +93,105 @@ function greetings(){
 
 const result1 = greetings()
 console.log(result1)
+
+
+
+// 2. Anonymous Function - Function Expression - A function which is declared without any name and that function will be stored inside a variable is known as Function Expression. That variable becomes the name of the function.
+
+// Syntax:
+/*
+
+let functionName = function(param: datatype(Optional)): returnType(Optional){
+        // Code to be executed
+        return .....
+}
+
+const result = functionName(argument)
+
+
+*/
+
+
+// Call back function - A function which will be utilised as a parameter of another function is know as call back function.
+
+
+let message = function(name:string, age:number){
+    console.log(`Your name is ${name} and age is ${age}`);
+}
+
+message("Rahul", 25);
+
+
+// 3. Arrow function (=>) - ES6 2015 - Lambda Function
+// 1. This function is also a part of anonymous function and will not have any name
+// 2. This function will be declared by using (=>) arrow symbol after the paranthesis.
+// 3. Arrow function use to shorten the number of lines of code
+// 4. If there is only one line of code then we can skip the curly braces {} and the return keyword. It implicity return the value from that function.
+// 5. If there is only one parameter for a function we can skip the paranthesis to define the parameter.
+
+//Syntax
+/*
+
+let variable = (param:datatype)=>{
+  // code
+  return
+}
+
+variable(argument)
+
+
+*/
+
+let add1 = (a:number,b:number)=>{
+    console.log(a-b);
+}
+
+add1(10,5);
+
+let sum1 = (a:number, b:number, c:number)=> (a+b+c) //{return a+b+b} 
+let sum1Result = sum1(10,20,30)
+console.log(sum1Result);
+
+
+let square = a => console.log(a**2)
+
+square(10)
+
+
+// Method Overloading - A method which have same name but different parameter is known as method overloading.
+
+// function name1(a:number){
+
+// }
+
+// function name1(a:number,b:number){
+
+// }
+
+// name1(1)
+// name1(12,34)
+
+// Default parameter and Optional parameter
+
+// Default parameter - A parameter which has a default value is known as Default parameter.
+
+function name1(a:number, b:number=5){
+   console.log(a+b);
+}
+
+name1(10)
+name1(10,20)
+
+// Optional Parameter (p?) - A parameter which may or may not be provided with a value during the function call 
+
+
+function display(name:string, age?:number){
+   console.log(name, age);
+}
+
+display("Rahul")
+display("Rahul", 30)
+
+// Assignment - Complete the above function by writing the logic to print only Name if age parameter has not value given.
+
+
