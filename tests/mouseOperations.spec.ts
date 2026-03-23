1500b0bf07ca2362cdcd587ca6311929df1507ee
Praveen <praveen.k@xnara.com>
2026-03-23 22:02:29 +0530

    addded MO

---
+++ tests/mouseOperations.spec.ts
@@ -0,0 +1,10 @@
+// click - click()
+// double click - dblclick()
+// right click - click({})
+// mouse hover - hover()
+// scroll down - 
+// drag and drop - locator.dragTo(Locator)
+
+import {test, expect} from '@playwright/test'
+
+test()
