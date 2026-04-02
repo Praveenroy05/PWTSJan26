a881bcb6bffe804a1aabad06087dd2a2cd895eb2
Praveen <praveen.k@xnara.com>
2026-04-02 22:59:10 +0530

    e2e

---
+++ tests/E2EAutomation.spec.ts
@@ -0,0 +1,57 @@
+let productName = "ZARA COAT 3"
+let email = "testnHNk@gmail.com"
+let country = " Singapore"
+
+
+import {test, expect} from '@playwright/test'
+
+test("E2E Automation scenario", async ({page})=>{
+    await page.goto("https://rahulshettyacademy.com/client")
+
+    await page.getByPlaceholder("email@example.com").clear()
+    await page.getByPlaceholder("email@example.com").fill(email)
+
+    await page.getByPlaceholder("enter your passsword").fill("Testing@1234")
+    await page.getByRole('button', {name:'Login'}).click()
+    await expect(page.locator(".fa-sign-out")).toBeVisible()
+
+    const products = page.locator("div.card-body") 
+
+    // Find the number of products are available on the page
+
+    // count() - Returns the number of element matching with the locator
+
+    await products.nth(0).waitFor()
+    // const countOfProduct = await products.count() // 3 [0-2] // 0,1,2 <3
+    // console.log(countOfProduct);
+
+    await products.filter({hasText:`${productName}`}).locator("button").last().click() 
+    
+    // div.card-body:has-text("iphone 13 pro") button
+    // for(let i=0; i<countOfProduct; i++){
+    //     const productText = await products.nth(i).locator("h5").textContent() // div.card-body h5
+    //     console.log(productText);
+    //     if(productText === productName){
+    //         await products.nth(i).locator("button").last().click()
+    //         break
+    //     }
+    // }
+    await expect(page.locator("#toast-container")).toHaveText("Product Added To Cart")
+
+    await page.locator("[routerlink='/dashboard/cart']").click()
+
+    await page.getByRole('button',{name:'Checkout'}).click()
+
+    await expect(page.locator("div.user__name input").first()).toHaveValue(email)
+    // pressSequentially() - It will enter char by char
+    await page.locator("div.user__name input").last().pressSequentially("in")
+
+    const ddResult  = page.locator("section.ta-results button")
+
+    await ddResult.nth(0).waitFor()
+
+    await ddResult.filter({hasText:`${country}`}).click()
+
+    await page.waitForTimeout(3000)
+
+})
