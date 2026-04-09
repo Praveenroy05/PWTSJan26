b88a3277b8dff36f08bafef4201761738d9ed34a
Praveen Roy <pkroy0505@gmail.com>
2026-04-09 22:13:06 +0530

    added hooks

--- tests/LoginPageTest.spec.ts
+++ tests/LoginPageTest.spec.ts
@@ -8,16 +8,17 @@ let email = "testnHNk@gmail.com"
 let password = "Testing@1234"
 let incorrectPwd = "Test"
 
-test("Valid login test", async ({page})=>{
+test.beforeEach(async ({page})=>{
     const lp = new LoginPage(page)
     await lp.launchURL(url)
+})
+
+test("Valid login test", async ()=>{
     await lp.loginIntoApplication(email, password)
     await expect(lp.homePageIdentifier).toBeVisible()
 })
 
-test("Invalid login test", async ({page})=>{
-    const lp = new LoginPage(page)
-    await lp.launchURL(url)
+test("Invalid login test", async ()=>{
     await lp.loginIntoApplication(email, incorrectPwd)
     await expect(lp.errorMessage).toHaveText("Incorrect email or password.")
 })
--- tests/hooks.spec.ts
+++ tests/hooks.spec.ts
@@ -1,1 +1,38 @@
 // Hooks - A special method which performs a setup and tear down process.
+
+// test.beforeAll() - It will be executed before running any of the test cases.- db connect
+// test.beforeEach() - It will run before running each and every test. - Common steps for all the test cases
+// test()
+// test.afterEach() - It will run after running each and every test. - 
+// test.afterAll()  - It will be executed after execution of all the test cases. close db connect
+
+
+import {test} from '@playwright/test'
+
+test.beforeAll(async ()=>{
+    console.log("Before ALl")
+})
+
+test.afterAll(async ()=>{
+    console.log("After ALl")
+})
+
+test.afterEach(async ()=>{
+    console.log("After each")
+})
+
+test.beforeEach(async ()=>{
+    console.log("Before Each")
+})
+
+test("Test1",async ()=>{
+    console.log("Test- 1")
+})
+
+test("Test2",async ()=>{
+    console.log("Test- 2")
+})
+
+test("Test3",async ()=>{
+    console.log("Test- 3")
+})
